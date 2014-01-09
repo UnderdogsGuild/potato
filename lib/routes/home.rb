2fc5a841dc40bdc7dfea14626175eb76ec7e9d45
Chris Thomas <cethomas1234@gmail.com>
2014-01-09 15:16:17 -0700

    Changing template directory structure, adding pages to home.rb. Creating login view.

--- lib/routes/home.rb
+++ lib/routes/home.rb
@@ -1,7 +1,14 @@
 class Application < Sinatra::Base
 
 	get '/' do
-		haml :'index'
+		haml :'pages/index'
 	end
 
+	get '/community/linkshells/doomfinders' do
+		haml :'pages/community/linkshells/doomfinders'
+	end
+
+	get '/tmp/login' do
+		haml :'views/login'
+	end
 end
