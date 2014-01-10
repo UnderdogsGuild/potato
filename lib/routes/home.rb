823444dcedc6e78feb464ea99538b9bd82a03ee4
Chris Thomas <cethomas1234@gmail.com>
2014-01-10 13:01:49 -0700

    Reorganized colors into master LESS filles, added error page, worked on DoomFinders

--- lib/routes/home.rb
+++ lib/routes/home.rb
@@ -5,10 +5,16 @@ class Application < Sinatra::Base
 	end
 
 	get '/community/linkshells/doomfinders' do
+		@less_pallet='/css/df_master.less'
 		haml :'pages/community/linkshells/doomfinders'
 	end
 
 	get '/tmp/login' do
 		haml :'views/login'
 	end
+
+	not_found do
+		haml :'pages/errors/404', :layout => :'errors'
+	end
+
 end
