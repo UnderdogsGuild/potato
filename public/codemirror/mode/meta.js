bc74191bb95f1e6e288a238f35c337a50c8283b4
Chris 'Faore' Thomas <cethomas1234@gmail.com>
2014-02-13 15:52:49 -0700

    Consolidated css and js to decrease load times.

--- public/codemirror/mode/meta.js
+++
@@ -1,5 +0,0 @@
-CodeMirror.modeInfo = [
-  {name: 'GitHub Flavored Markdown', mime: 'text/x-gfm', mode: 'gfm'},
-  {name: 'Markdown (GitHub-flavour)', mime: 'text/x-markdown', mode: 'markdown'},
-  {name: 'XML', mime: 'application/xml', mode: 'xml'}
-];
