4a7ac1c5990e32af080099c4b4569814e8aff868
Michishige Kaito <me@mkaito.com>
2014-03-05 02:13:31 +0000

    Further tests for the thread view

--- spec/routes/forum_spec.rb
+++ spec/routes/forum_spec.rb
@@ -9,31 +9,110 @@ feature "/community/underdogs/forum" do
 		any_instance_of(Application) do |a|
 			stub(a).user { @user }
 		end
+		stub(@user).can? { false }
 		stub(@user).can?(:view_forum_threads) { true }
 	end
 
 	feature "index" do
 		scenario "shows a list of threads" do
-			stub(@user).can?(:view_officer_threads) { false }
-
 			visit "/community/underdogs/forum/"
-			expect(page).to_not have_text("I'm afraid there are no threads for you")
 			expect(page).to have_selector("tr.thread", count: 2)
 		end
 
 		scenario "shows officer threads to officers" do
 			stub(@user).can?(:view_officer_threads) { true }
-
 			visit "/community/underdogs/forum/"
-			expect(page).to have_selector("tr.thread", count: 4)
 			expect(page).to have_selector("tr.thread.officer", count: 2)
 		end
 	end
 
 	feature "thread view" do
-		scenario "finds an existing thread" do
+		scenario "finds an existing thread and displays all its posts" do
 			@thread = ForumThread.first
 			visit @thread.url
+			expect(page).to have_text(@thread.title)
+			expect(page).to have_selector(".ud-post", count: @thread.posts.count)
+		end
+
+		feature "moderation tools" do
+			scenario "are not displayed for non-moderators" do
+				@thread = ForumThread.first
+				visit @thread.url
+				expect(page).to_not have_selector(".moderate")
+			end
+
+			scenario "are displayed for moderators" do
+				stub(@user).can?(:moderate_forum) { true }
+				@thread = ForumThread.first
+				visit @thread.url
+				expect(page).to have_selector(".moderate", count: @thread.posts.count)
+			end
+		end
+
+		feature "user badges" do
+			scenario "for a Top Dog are displayed" do
+				@thread = ForumThread.first
+				any_instance_of(User) do |u|
+					stub(u).has_role? { false }
+					stub(u).has_role?("topdog") { true }
+				end
+				visit @thread.url
+				expect(page).to have_selector(".ud-rank.topdog")
+			end
+
+			scenario "for a Watchdog are displayed" do
+				@thread = ForumThread.first
+				any_instance_of(User) do |u|
+					stub(u).has_role? { false }
+					stub(u).has_role?("watchdog") { true }
+				end
+				visit @thread.url
+				expect(page).to have_selector(".ud-rank.watchdog")
+			end
+
+			scenario "for an Underdog are displayed" do
+				@thread = ForumThread.first
+				any_instance_of(User) do |u|
+					stub(u).has_role? { false }
+				end
+				visit @thread.url
+				expect(page).to have_selector(".ud-rank.underdog")
+			end
+
+			scenario "only display the highest rank" do
+				@thread = ForumThread.first
+				any_instance_of(User) do |u|
+					stub(u).has_role? { false }
+					stub(u).has_role?("topdog") { true }
+					stub(u).has_role?("watchdog") { true }
+				end
+				visit @thread.url
+				expect(page).to have_selector(".ud-rank.topdog")
+				expect(page).to_not have_selector(".ud-rank.watchdog")
+				expect(page).to_not have_selector(".ud-rank.underdog")
+			end
+
+			scenario "for an Admin are displayed" do
+				@thread = ForumThread.first
+				any_instance_of(User) do |u|
+					stub(u).has_role? { false }
+					stub(u).has_role?("admin") { true }
+				end
+				visit @thread.url
+				expect(page).to have_selector(".ud-rank.admin")
+			end
+
+			scenario "for an Admin are displayed alongside others" do
+				@thread = ForumThread.first
+				any_instance_of(User) do |u|
+					stub(u).has_role? { false }
+					stub(u).has_role?("admin") { true }
+					stub(u).has_role?("watchdog") { true }
+				end
+				visit @thread.url
+				expect(page).to have_selector(".ud-rank.admin")
+				expect(page).to have_selector(".ud-rank.watchdog")
+			end
 		end
 	end
 end
