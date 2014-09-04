cc8321b634766241df69d79517837bf6b892f172
Michishige Kaito <me@mkaito.com>
2014-09-04 18:33:08 +0100

    Rename general template folder
    Site templates are now stored in templates/site, instead of templates/pages.
    This is because templates/pages will be used for the Pages module.

--- lib/routes/home.rb
+++ lib/routes/home.rb
@@ -1,7 +1,7 @@
 class Application < Sinatra::Base
 
 	get '/' do
-		haml :'pages/index'
+		haml :'site/index'
 	end
 
 	#get '/community/linkshells/doomfinders' do
@@ -9,27 +9,27 @@ class Application < Sinatra::Base
 		#haml :'pages/community/linkshells/doomfinders'
 	#end
 
-	get '/donate/success' do
-		haml :'pages/donate/success'
-	end
+	# get '/donate/success' do
+	# 	haml :'site/donate/success'
+	# end
 
-	get '/donate/cancel' do
-		haml :'pages/donate/cancel'
-	end
+	# get '/donate/cancel' do
+	# 	haml :'site/donate/cancel'
+	# end
 
-	get '/tmp/403' do
-		haml :'pages/errors/403', layout: :errors		
-	end
+	# get '/tmp/403' do
+	# 	haml :'site/errors/403', layout: :errors		
+	# end
 
-	get '/tmp/new_post' do
-		haml :'forum/new_post'
-	end
+	# get '/tmp/new_post' do
+	# 	haml :'forum/new_post'
+	# end
 
-	get '/tmp/new_thread' do
-		haml :'forum/new_thread'
-	end
+	# get '/tmp/new_thread' do
+	# 	haml :'forum/new_thread'
+	# end
 
-	get '/tmp/edit_post' do
-		haml :'forum/edit_post'
-	end
+	# get '/tmp/edit_post' do
+	# 	haml :'forum/edit_post'
+	# end
 end
